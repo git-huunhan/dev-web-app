6e569e59c7b7d56968539b9bc9a10eeedd8c3d84
Huu Nhan <huunhankirigamer@gmail.com>
2020-10-01 15:38:40 +0700

    Web Application Development

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,30 +5,19 @@ import {
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
-import avatar from '../img/avatar.jpg'
+import avatar from '../img/avatar.jpg';
+import wall from '../img/wall.png';
 
 class Index extends Component {
   render(){
     return(
-      <Col className="mb-4 d-flex" sm="12" md="12" lg="12" xl="12">
-        <Col className="mb-4" sm="12" md="12" lg="12" xl="3">
-          <Container>
-            <div>
-              <Card id="card" className="profile">
-                <CardBody >
-                  <CardTitle id="card-title" className="font-weight-bold">Giới thiệu</CardTitle>
-                  <CardText>Học Công Nghệ Thông Tin tại Trường Đại học Đà Lạt</CardText>
-                  <CardText>Đã học tại Trường THPT Đức Trọng - Lâm Đồng</CardText>
-                  <CardText>Sống tại Đà Lạt</CardText>
-                </CardBody>
-              </Card>
+      <Col className="p-0">
+          <Container className="p-0 mb-4 mw-100">
+            <div className="wall-container">
+              <img className="mw-100 wall-img" src={wall} alt="wallpaper"/>
             </div>
-          </Container>
-        </Col>
-        <Col className="m-0" sm="12" md="12" lg="12" xl="6">
-          <Container>
             <div>
-              <Card id="card" className="profile">
+              <Card id="card" className="card-wall">
                 <img className="avatar mx-auto" src={avatar} alt="avatar" />
                 <CardBody className="mx-auto">
                   <CardTitle id="card-title" className="text-center font-weight-bold">Izu Cute Lovely</CardTitle>
@@ -42,23 +31,41 @@ class Index extends Component {
               </Card>
             </div>
           </Container>
+        
+
+        <Col className="d-flex mb-4 justify-content-center" sm="12" md="12" lg="12" xl="12">
+          <Col className="p-0" sm="12" md="12" lg="12" xl="4">
+            <Container>
+              <div className="text-color">
+                <Card id="card" className="profile">
+                  <CardBody>
+                    <CardTitle id="card-title" className="font-weight-bold h5">Giới thiệu</CardTitle>
+                    <CardText><i class="fas fa-graduation-cap"></i> Học <b>Công Nghệ Thông Tin</b> tại <b>Trường Đại học Đà Lạt</b></CardText>
+                    <CardText><i class="fas fa-graduation-cap"></i> Đã học tại <b>Trường THPT Đức Trọng - Lâm Đồng</b></CardText>
+                    <CardText><i class="fas fa-home"></i> Sống tại <b>Đà Lạt</b></CardText>
+                  </CardBody>
+                </Card>
+              </div>
+            </Container>
+          </Col>
+
+          <Col className="p-0" sm="12" md="12" lg="12" xl="6">
+            <Container>
+              <div>
+                <Card id="card" className="profile">
+                  <CardBody className="mx-auto">
+                    <CardTitle id="card-title" className="text-center font-weight-bold">Izu Cute Lovely</CardTitle>
+                    <CardText>📖 ʙᴇᴛᴛᴇʀ ʟᴀᴛᴇ ᴛʜᴀɴ ɴᴇᴠᴇʀ ⌨️</CardText>
+                    <Col className="d-flex justify-content-center" sm="6" md="4" lg="3" xl="12">
+                    </Col>
+                  </CardBody>
+                </Card>
+              </div>
+            </Container>
+          </Col>
         </Col>
 
-        <Col className="mb-4" sm="12" md="12" lg="12" xl="3">
-          <Container>
-            <div>
-              <Card id="card" className="profile">
-                <CardBody className="mx-auto">
-                  <CardTitle id="card-title" className="text-center font-weight-bold">Izu Cute Lovely</CardTitle>
-                  <CardText>📖 ʙᴇᴛᴛᴇʀ ʟᴀᴛᴇ ᴛʜᴀɴ ɴᴇᴠᴇʀ ⌨️</CardText>
-                  <Col className="d-flex justify-content-center" sm="6" md="4" lg="3" xl="12">
-                 
-                  </Col>
-                </CardBody>
-              </Card>
-            </div>
-          </Container>
-        </Col>
+        
       </Col>
       
     );
--- src/sub-pages/LabDomain.js
+++ src/sub-pages/LabDomain.js
@@ -9,8 +9,14 @@ class LabDomain extends Component {
     this.state = {
       products: [{
         "id": "95ad242b-03c4-4758-a8e4-cd0b0379bf12",
-        "name": "Wine - Casablanca Valley",
-        "description": "tempus vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est risus",
+        "name": "1. Tổng quan về DNS",
+        "des": "DNS là Domain Name System, hệ thống tên miền hay Hệ thống phân giải (resolve) tên miền",
+        "description": "DNS là Domain Name System, hệ thống tên miền hay Hệ thống phân giải (resolve) tên miền.DNS dùng để lưu trữ các tên miền (domain name, ví dụ www.google.com), cung cấp dịch vụ chuyển đổi từ địa chỉ dạng chuỗi (tên miền) sang địa chỉ dạng số (IP) và ngược lại (ví dụ www.google.com <> 216.58.221.228)",
+        "imageUrl": "https://webhostingtrace.com/wp-content/uploads/2008/03/domain-and-hosting1.jpg"
+      }, {
+        "id": "95ad242b-03c4-4758-a8e4-cd0b0379bf12",
+        "name": "2. Tên miền cấp 1, 2, 3",
+        "des": "Tên miền cấp 1 cũng là tên miền quốc tế, được dùng chung cho nhiều quốc gia, mỗi tên miền đại diện cho một lĩnh vực, một ngành nghề, hay một khu vực địa lý. Ví dụ: .com, .net, .org, .mil, .edu, .gov, .asia, .eu",
         "imageUrl": "https://webhostingtrace.com/wp-content/uploads/2008/03/domain-and-hosting1.jpg"
       }]
     }; 
@@ -27,16 +33,10 @@ class LabDomain extends Component {
             { products.map(products =>(
               <Col className="mb-4" sm="12" md="12" lg="12" xl="12">
                 <Card id="card" className="border-0 h-100 flex-row">
-                  <Col className="m-4" sm="6" md="4" lg="3" xl="4">
-                    <CardImg
-                    className=""
-                    id="card-img" 
-                    src={products.imageUrl}
-                    alt="Card image cap" />
-                  </Col>
                   <Col className="m-4">
                     <CardBody className="d-flex flex-column">
                       <CardTitle id="card-title">{products.name}</CardTitle>
+                      <CardText>{products.des}</CardText>
                       <CardText>{products.description}</CardText>
                     </CardBody>
                   </Col>
